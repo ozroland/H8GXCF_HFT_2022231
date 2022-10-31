71a950d24cc49ff11053c539f07c7114c9d7ce8f
ozroland <ozroland@griffsoft.com>
2022-10-31 13:49:18 +0100

    Default logic layer items created

---
+++ H8GXCF_HFT_2022231.Logic/Interfaces/IMemberLogic.cs
@@ -0,0 +1,18 @@
+﻿using H8GXCF_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H8GXCF_HFT_2022231.Logic.Interfaces
+{
+    public interface IMemberLogic
+    {
+        void Create(Member member);
+        void Delete(int id);
+        IEnumerable<Member> ReadAll();
+        Member Read(int id);
+        void Update(Member member);
+    }
+}
---
+++ H8GXCF_HFT_2022231.Logic/Services/MemberLogic.cs
@@ -0,0 +1,44 @@
+﻿using H8GXCF_HFT_2022231.Logic.Interfaces;
+using H8GXCF_HFT_2022231.Models;
+using H8GXCF_HFT_2022231.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H8GXCF_HFT_2022231.Logic.Services
+{
+    public class MemberLogic : IMemberLogic
+    {
+        IMemberRepository memberRepository;
+        public MemberLogic(IMemberRepository memberRepository)
+        {
+            this.memberRepository = memberRepository;
+        }
+        public void Create(Member member)
+        {
+            memberRepository.Create(member);
+        }
+
+        public void Delete(int id)
+        {
+            memberRepository.Delete(id);
+        }
+
+        public Member Read(int id)
+        {
+           return memberRepository.Read(id);
+        }
+
+        public IEnumerable<Member> ReadAll()
+        {
+            return memberRepository.ReadAll();
+        }
+
+        public void Update(Member member)
+        {
+           memberRepository.Update(member);
+        }
+    }
+}
